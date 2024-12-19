ac1dee219e815f93bdbfd6d2366900a7798f9e04
BuildTools <unconfigured@null.spigotmc.org>
2024-12-19 14:54:19 +0100

    Correction poisitin fin

--- src/c#/Credits.cs
+++ src/c#/Credits.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -7,9 +8,13 @@ public class Credits
 {
     private Sprite YouWin;
     Button _quitButton;
+    SpriteFont _font;
+    private Array f;
 
     public Credits()
     {
+        _font =  Utils._content.Load<SpriteFont>("MyMenuFont");
+        f = Felicitations.GetValues(typeof(Felicitations));
         YouWin = new Sprite("YouWin",new Vector2(Utils.screenWidth/2,150),50, 250);
         _quitButton = new Button("Exit_BTN",new Vector2(Utils.screenWidth/2,300),50, 150);
     }
@@ -18,6 +23,7 @@ public class Credits
     {
         YouWin.Draw(spriteBatch);
         _quitButton.Draw(spriteBatch);
+        spriteBatch.DrawString(_font,f.GetValue(Utils._currentScore%4).ToString(), new Vector2(Utils.screenWidth/4,Utils.screenHeight/8), Color.White);
     }
 
     public void Update(GameTime gameTime)
